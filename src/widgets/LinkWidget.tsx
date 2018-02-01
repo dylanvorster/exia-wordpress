ca62343c7e7eb1fc4feb94e0ce03071e53e6f13f
Dylan Vorster <dylanvorster.dv@gmail.com>
2018-02-01 23:06:42 +0200

    fixup links

--- src/widgets/LinkWidget.tsx
+++ src/widgets/LinkWidget.tsx
@@ -1,10 +1,15 @@
 import {Link} from "react-router-dom";
 import * as React from "react";
+import * as _ from "lodash";
+import * as url from "url";
 
 export class LinkWidget extends Link{
     render(){
+        let link:string = (this.props.to || "") as any;
+        let base = url.parse(link);
+
         // internal link
-        if((this.props.to as any).indexOf(window.location.origin) !== -1){
+        if(_.startsWith(link, "/") || window.location.origin.indexOf(base.host) !== -1){
             return <Link {...this.props} to={(this.props.to as any).replace(window.location.origin, "")} />
         }
 

61e7ef44b8f4684957a8a68329c6f3dcb0b2f69b
Dylan Vorster <dylanvorster.dv@gmail.com>
2018-02-03 20:59:32 +0200

    added auto tracking

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,21 @@ require("./sass/main.scss");
 require("typeface-open-sans");
 require("font-awesome/css/font-awesome.min.css");
 
+import 'autotrack';
+
 /**
  * @author Dylan Vorster
  */
 document.addEventListener("DOMContentLoaded", (event) => {
     let app = new Application();
     app.run();
+
+    // google analytics if defined
+    if((window as any).ga){
+        (window as any)('require', 'eventTracker');
+        (window as any)('require', 'outboundLinkTracker');
+        (window as any)('require', 'urlChangeTracker');
+    }
 
     ReactDOM.render(
         <BrowserRouter>
